3f171e5f6de4c88aab33826ffd756bf827f49f29
maria-zhynko <66383351+maria-zhynko@users.noreply.github.com>
2020-10-19 22:06:19 +0300

    feat: complete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let brackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    let anotherSTR = str;
+    while (true) {
+        for (let i = 0; i < brackets.length; i++) {
+            str = str.replace(brackets[i], '');
+        }
+        if (str.length === 0) {
+            return true;
+        }
+        if (anotherSTR === str) {
+            return false;
+        }
+        
+        anotherSTR = str;
+    }
 }
